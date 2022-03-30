018869f805fa74fa91acde7ccab759ae8b17aaf7
Jannat-Monnisha <101254656+Jannat-Monnisha@users.noreply.github.com>
2022-03-31 01:28:03 +0600

    jannat

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -1,14 +1,46 @@
+//jannat
 package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity<ActivityMainBinding> extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding =DataBindingUtil.setContenView(activity this,R.layout.activity_main);
+    }
+    private void setUpwithViewpager(viewpager viewpager){
+      MainActivity.SectionspagerAdapter adapter=new SectionspagerAdapter(getSupportFragmentManager());
+      //we need 3 fragment
+        viewpager.setAdapter(adapter);
+    }
+    //add this code
+    private static class SectionspagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final list<String> mFragmentTittleList = new ArrayList<>();
+
+        public SectionspagerAdapter(FragmentManager manager) {super(manager) ;}
+        @Override
+        public Fragment getIteam(int position){return mFragmentList.get(position);}
+        @Override
+        public  int getCount(){return mFragmentList.size();}
+        public void addFragment(fragment,String ,little) {
+            mFragmentList.add(fragment);
+            mFragmentTittleListList.add(title)
+        }
+
+        @Override
+        public charSequence getpageTittle(int position) {return mFragmentTittleListList.grt(position);}
     }
 }
