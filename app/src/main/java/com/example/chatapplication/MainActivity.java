b256cfc7a1f05ac68f3afaf4a5c8bd660402eb48
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-04-10 13:26:56 +0600

    anik

--- app/src/main/java/com/example/chatapplication/MainActivity.java
+++ app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -31,28 +32,32 @@ public class MainActivity extends AppCompatActivity {
         setUpWithViewPager(binding.viewPager);
         binding.tabLayout.setupWithViewPager(binding.viewPager);
         setSupportActionBar(binding.toolbar);
+
+        binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                changeFabICon(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
-//sumi
-    bindig.viewpage.addOnPageChangeiistener(new viewPager.OnPageChangeiistener(){
-        @Override
-                public void OnPagescrolled(int position, float positionOffset, int positionOffsetpixels)
-        }
-        @Override
-    public void OnPagescrolled(int position) {
-        changFabICon(posion);
-        }
-        @Override
-    public void OnPagescrollstateChanged(int state) {
-        }
-        }};
+
     private void setUpWithViewPager(ViewPager viewPager){
-      MainActivity.SectionsPagerAdapter adapter=new SectionsPagerAdapter(getSupportFragmentManager());
+      MainActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
       adapter.addFragment(new ChatsFragment(), "Chats");
       adapter.addFragment(new StatusFragment(), "Status");
       adapter.addFragment(new CallsFragment(), "Calls");
-
-        //we need 3 fragments
-        viewPager.setAdapter(adapter);
+      viewPager.setAdapter(adapter);
     }
 
     //add this code
@@ -92,26 +97,25 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         switch (id){
-            case R.id.menu_search: Toast.makeText(MainActivity.this, "Action Search", Toast.LENGTH_LONG).show(); break;
-            case R.id.menu_more: Toast.makeText(MainActivity.this, "Action More", Toast.LENGTH_LONG).show(); break;
+            case R.id.menu_search : Toast.makeText(MainActivity.this, "Action Search", Toast.LENGTH_LONG).show(); break;
+            case R.id.menu_more : Toast.makeText(MainActivity.this, "Action More", Toast.LENGTH_LONG).show(); break;
         }
         return super.onOptionsItemSelected(item);
     }
-    //sumi
-    private void changFabICon(final int index)
-        binding.fabAction.hide()
-                new handler().postDelayed(new Runnable() {
-        @Override
-        public void run () {
-            switch (index) {
-                case 0: binding.fabAction.setImagedrawable(getDrawable(R.drawable.ic_chat_black_24));break;
-                case 1: binding.fabAction.setImagedrawable(getDrawable(R.drawable.ic_camera_black_24));break;
-                case 2: binding.fabAction.setImagedrawable(getDrawable(R.drawable.ic_call_black_24));break;
+
+    private void changeFabICon(final int index){
+        binding.fabAction.hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (index){
+                    case 0 : binding.fabAction.setImageDrawable(getDrawable(R.drawable.ic_baseline_chat_24)); break;
+                    case 1 : binding.fabAction.setImageDrawable(getDrawable(R.drawable.ic_baseline_camera_24)); break;
+                    case 2 : binding.fabAction.setImageDrawable(getDrawable(R.drawable.ic_baseline_call_24)); break;
+                }
+                binding.fabAction.show();
             }
-            binding.fabAction.show();
-        }
-    }.delaymillis 400);
+        },400);
     }
-
-
 }
