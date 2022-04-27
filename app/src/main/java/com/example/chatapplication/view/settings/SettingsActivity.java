a1b7a84ca9064eaa4e8d979a65490b7b949cb030
Sumi-cyber <101254702+Sumi-cyber@users.noreply.github.com>
2022-04-27 22:10:06 +0600

    sumi

--- app/src/main/java/com/example/chatapplication/view/settings/SettingsActivity.java
+++ app/src/main/java/com/example/chatapplication/view/settings/SettingsActivity.java
@@ -1,16 +1,50 @@
+//sumi
 package com.example.chatapplication.view.settings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.chatapplication.R;
+import com.example.chatapplication.databinding.ActivitySetUserInfoBinding;
+import com.example.chatapplication.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
+    private ActivitySettingsBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestor Firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = DataBindingUtil.setContentView(activity this,R.layout.activity_settings);
+
+        firebaseUser= firebasefirestore.getInstance();
+        firebaseUser= Firebaseauth.getInstance(.getCurrentUser();
+
+        if (firebaseUser!=null){
+        getInfo();
+
+       }
     }
+
+    private void getInfo(){
+        firestore.collection(collectionpath "Users").document(firebaseUser.getUid()).get().addonSuccessListener(new addonSuccessListener<DocumentSnapshot> (){
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot){
+                String userName = objects.requirNoNull( documentSnapshot.get('userName')).toString();
+                binding.tvUsername.setText(userName);
+
+        }
+    }).addonSuccessListener(new OnfailureListener(){
+        @Override
+                public void onfailure(@NonNull Exception e){
+                log.d(tag: "get data",msg: "onfailure:"+e.getMessage());
+        }
+    });
+}
 }
