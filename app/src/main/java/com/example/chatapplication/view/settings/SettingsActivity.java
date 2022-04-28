9401208eba6fd0479a3b30792d0c31cc1543bb07
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-04-29 01:38:59 +0600

    anik

--- app/src/main/java/com/example/chatapplication/view/settings/SettingsActivity.java
+++ app/src/main/java/com/example/chatapplication/view/settings/SettingsActivity.java
@@ -6,45 +6,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.chatapplication.R;
 import com.example.chatapplication.databinding.ActivitySetUserInfoBinding;
 import com.example.chatapplication.databinding.ActivitySettingsBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class SettingsActivity extends AppCompatActivity {
 
 
     private ActivitySettingsBinding binding;
     private FirebaseUser firebaseUser;
-    private FirebaseFirestor Firestore;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(activity this,R.layout.activity_settings);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_settings);
 
-        firebaseUser= firebasefirestore.getInstance();
-        firebaseUser= Firebaseauth.getInstance(.getCurrentUser();
+        firestore = FirebaseFirestore.getInstance();
+        firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
 
         if (firebaseUser!=null){
-        getInfo();
-
+            getInfo();
        }
     }
 
-    private void getInfo(){
-        firestore.collection(collectionpath "Users").document(firebaseUser.getUid()).get().addonSuccessListener(new addonSuccessListener<DocumentSnapshot> (){
+    private void getInfo() {
+        firestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot){
-                String userName = objects.requirNoNull( documentSnapshot.get('userName')).toString();
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String userName = Objects.requireNonNull(documentSnapshot.get("userName")).toString();
                 binding.tvUsername.setText(userName);
 
-        }
-    }).addonSuccessListener(new OnfailureListener(){
-        @Override
-                public void onfailure(@NonNull Exception e){
-                log.d(tag: "get data",msg: "onfailure:"+e.getMessage());
-        }
-    });
-}
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Get Data", "onFailure:" + e.getMessage());
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/chatapplication/view/startup/SplashScreenActivity.java
+++ app/src/main/java/com/example/chatapplication/view/startup/SplashScreenActivity.java
@@ -11,14 +11,15 @@ import com.example.chatapplication.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-@SuppressLint("CustomSplashScreen")
 public class SplashScreenActivity extends AppCompatActivity {
+
     FirebaseUser firebaseUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        firebaseUser = FirebaseAuth.getInstance();
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         if (firebaseUser!=null){
             new Handler().postDelayed(new Runnable() {
                 @Override
