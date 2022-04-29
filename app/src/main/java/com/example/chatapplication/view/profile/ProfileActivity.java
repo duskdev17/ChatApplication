54218f2115552b91033ef690e13453d869323d58
Jannat-Monnisha <101254656+Jannat-Monnisha@users.noreply.github.com>
2022-04-29 11:57:19 +0600

    Jannat

--- app/src/main/java/com/example/chatapplication/view/profile/ProfileActivity.java
+++ app/src/main/java/com/example/chatapplication/view/profile/ProfileActivity.java
@@ -1,16 +1,60 @@
 package com.example.chatapplication.view.profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.example.chatapplication.R;
+import com.example.chatapplication.databinding.ActivityProfileBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private ActivityProfileBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_profile);
+
+        setSupportActionBar(binding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firestore = FirebaseFirestore.getInstance();
+        if (firebaseUser!=null){
+
+            getInfo();
+        }
+
+    }
+
+    private void getInfo() {
+        firestore.collection("User").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String username = documentSnapshot.getString(field:"userName");
+                String userPhone = documentSnapshot.getString(field:"userPhone");
+                binding.tvUsername.setText(username);
+                binding.tvPhone.setText(userPhone);
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chatapplication/view/settings/SettingsActivity.java
+++ app/src/main/java/com/example/chatapplication/view/settings/SettingsActivity.java
@@ -5,12 +5,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.chatapplication.R;
 import com.example.chatapplication.databinding.ActivitySetUserInfoBinding;
 import com.example.chatapplication.databinding.ActivitySettingsBinding;
+import com.example.chatapplication.view.profile.ProfileActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,6 +41,17 @@ public class SettingsActivity extends AppCompatActivity {
         if (firebaseUser!=null){
             getInfo();
        }
+        initClickAction();
+    }
+
+    private void initClickAction() {
+        binding.InProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SettingsActivity.this, ProfileActivity.class));
+
+            }
+        };
     }
 
     private void getInfo() {
