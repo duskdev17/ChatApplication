62108effe724ced7561dbfc42fa34b618cfcb9c8
Sumi-cyber <101254702+Sumi-cyber@users.noreply.github.com>
2022-04-29 21:40:15 +0600

    sumi

--- app/src/main/java/com/example/chatapplication/view/profile/ProfileActivity.java
+++ app/src/main/java/com/example/chatapplication/view/profile/ProfileActivity.java
@@ -2,14 +2,20 @@ package com.example.chatapplication.view.profile;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.databinding.DataBindingUtil;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.chatapplication.R;
 import com.example.chatapplication.databinding.ActivityProfileBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -22,6 +28,11 @@ public class ProfileActivity extends AppCompatActivity {
     private ActivityProfileBinding binding;
     private FirebaseUser firebaseUser;
     private FirebaseFirestore firestore;
+
+    private BottomSheetDialog bottomSheetDialog;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +41,9 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(binding.toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         firestore = FirebaseFirestore.getInstance();
         if (firebaseUser!=null){
@@ -37,8 +51,40 @@ public class ProfileActivity extends AppCompatActivity {
             getInfo();
         }
 
+        initaActionClick();
+
     }
 
+    private void initaActionClick() {
+        binding.facCamera.setOnCamera.setOnClickListener(new view.onclickListener(){
+            @Override
+            public void onClick(view v) {
+                showBottomsheetpickphoto();
+            }
+            }
+        });
+    }
+
+private void showBottomsheetpickphoto() {
+    @SuppressLint("inflateParams") view view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick, null);
+
+    bottomSheetDialog= new bottomSheetDialog(context: this);
+    bottomSheetDialog.setContentView(view);
+
+    if (build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        objects.requireNonNull(bottomSheetDialog.getWindow()).addFlags(WindowManager.layoutParams.FLAG_TRANSLUCENT_STATUS);
+
+    }
+
+    bottomSheetDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            bottomSheetDialog = null;
+
+        }
+    });
+    bottomSheetDialog.show();
+}
     private void getInfo() {
         firestore.collection("User").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
